43d1e104c2bc1c82acb627f9e8eb6eb7954efdfa
Nick Dimov <dimoffnd@gmail.com>
2019-12-19 16:39:35 +0300

    fixes

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -4,17 +4,7 @@ class TodosController < ApplicationController
     @projects = Project.all
     respond_to do |format|
       format.html
-      format.json { 
-        projects = []
-        @projects.each do |project|
-          todos = []
-            project.todos.each do |todo|
-              todos << {id: todo.id,text: todo.text, isCompleted: todo.isCompleted}
-            end
-          projects << {id: project.id, title: project.title, todos: todos }
-        end
-        render json: projects
-      }
+      format.json { render json: @projects, include: [:todos] }
     end
         
         
